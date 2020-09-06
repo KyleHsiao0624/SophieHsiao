25af8d100497274e8d56c76818aa450e5d6f85af
KyleHsiao0624 <70419315+KyleHsiao0624@users.noreply.github.com>
2020-09-07 00:45:39 +0800

    Create MVC5 CURD
    New

---
+++ ASP.NET MVC 5 CURD/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using ASP.NET_MVC_5_CURD.Infrastructure;
+
+namespace ASP.NET_MVC_5_CURD.App_Start
+{
+    public class FilterConfig
+    {
+        public static void Configure(System.Web.Mvc.GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ASP.NET MVC 5 CURD/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ASP.NET_MVC_5_CURD.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
